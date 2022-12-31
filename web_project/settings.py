2729663ebb81bcb3e03048c1f402ebf1a3b5d2c9
Gift <sokogfb@gmail.com>
2022-12-31 22:47:40 +0200

    Mysql Added

--- web_project/settings.py
+++ web_project/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'intimeparkers',
+        'USER': 'bbievents',
+        'PASSWORD': 'MGICZambia@2022@',
+        'HOST': 'bbievents.mysql.pythonanywhere-services.com',
+        'PORT': 3306
     }
 }
 
