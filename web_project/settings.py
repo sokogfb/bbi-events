2fcc5022a61da481e65deb79a90f27214b8f0ae5
Gift <sokogfb@gmail.com>
2022-12-31 22:14:34 +0200

    Added Static Root

--- web_project/settings.py
+++ web_project/settings.py
@@ -116,6 +116,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATIC_ROOT = os.path.join('staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
